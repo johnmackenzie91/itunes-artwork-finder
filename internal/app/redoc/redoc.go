5696f85d572904dfd8fd7a3de5262b5a714d7d84
John Mackenzie <johnmackenziewebdeveloper@gmail.com>
2021-09-12 19:51:36 +0100

    feat: added handler endpoint

---
+++ internal/app/redoc/redoc.go
@@ -0,0 +1,22 @@
+package redoc
+
+import (
+	"net/http"
+)
+
+//go:embed docs/*
+//var docs embed.FS
+
+// V1Docs returns the page that will host the specifiction
+func V1Docs() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
+
+// V1Spec returns the sepcification for v1 version of the api
+func V1Spec() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
