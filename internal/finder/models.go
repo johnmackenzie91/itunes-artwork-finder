362f606bab7ec050f90d2c98c36a79c2a934ff06
John Mackenzie <johnmackenziewebdeveloper@gmail.com>
2021-09-17 16:09:46 +0100

    refactor(itunes): changed wrapper to func and not struct

---
+++ internal/finder/models.go
@@ -0,0 +1,14 @@
+package finder
+
+// SearchResponse is the response that will be returned by all finders. This bring the response into our domain
+type SearchResponse struct {
+	ResultCount uint `json:"resultCount"`
+	Results     []Result
+}
+
+// Result represents each single result
+type Result struct {
+	Artist         string
+	CollectionName string
+	Link           string
+}
